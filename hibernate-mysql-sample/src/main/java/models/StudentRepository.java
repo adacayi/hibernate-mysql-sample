ad22d0ae0835d32de10db9b377d6aad9610991af
Abdullah Sanver <a_sanver@hotmail.com>
2018-07-26 19:27:12 +0100

    In findByColumns method, null address criterion is also taken into consideration.

--- hibernate-mysql-sample/src/main/java/models/StudentRepository.java
+++ hibernate-mysql-sample/src/main/java/models/StudentRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	List<Student> findByNameOrAge(String name, int age);
 
-	@Query("select s from Student s where (?1 is null or s.name like ?1) and s.address like ?2 and (?3=-1 or ?3<=s.age) and (?4=-1 or ?4>=s.age)")
+	@Query("select s from Student s where (?1 is null or s.name like ?1) and (s.address is null or s.address like ?2) and (?3=-1 or ?3<=s.age) and (?4=-1 or ?4>=s.age)")
 	List<Student> findByColumns(String name, String address, int minAge, int maxAge);
 
 	@Query("select (case when length(ltrim(rtrim(s.name)))>0 then concat(s.name,' Bey') else s.name end) from Student s where s.address like ?1 and (?2=-1 or ?2<=s.age) and (?3=-1 or ?3>=s.age)")
